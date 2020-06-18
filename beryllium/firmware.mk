fc6f4fca3fa1561d3e2aba755387ef88cf74e12c
HPinho <57301090+HPinho@users.noreply.github.com>
2020-06-18 10:32:25 -0300

    Add files via upload

---
+++ beryllium/firmware.mk
@@ -0,0 +1,18 @@
+# Firmware
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi-firmware/beryllium/abl.elf:install/firmware-update/abl.elf \
+    vendor/xiaomi-firmware/beryllium/aop.img:install/firmware-update/aop.img \
+    vendor/xiaomi-firmware/beryllium/bluetooth.img:install/firmware-update/bluetooth.img \
+    vendor/xiaomi-firmware/beryllium/cmnlib.img:install/firmware-update/cmnlib.img \
+    vendor/xiaomi-firmware/beryllium/cmnlib64.img:install/firmware-update/cmnlib64.img \
+    vendor/xiaomi-firmware/beryllium/devcfg.img:install/firmware-update/devcfg.img \
+    vendor/xiaomi-firmware/beryllium/dsp.img:install/firmware-update/dsp.img \
+    vendor/xiaomi-firmware/beryllium/hyp.img:install/firmware-update/hyp.img \
+    vendor/xiaomi-firmware/beryllium/keymaster.img:install/firmware-update/keymaster.img \
+    vendor/xiaomi-firmware/beryllium/logo.img:install/firmware-update/logo.img \
+    vendor/xiaomi-firmware/beryllium/modem.img:install/firmware-update/modem.img \
+    vendor/xiaomi-firmware/beryllium/qupfw.img:install/firmware-update/qupfw.img \
+    vendor/xiaomi-firmware/beryllium/storsec.img:install/firmware-update/storsec.img \
+    vendor/xiaomi-firmware/beryllium/tz.img:install/firmware-update/tz.img \
+    vendor/xiaomi-firmware/beryllium/xbl_config.img:install/firmware-update/xbl_config.img \
+    vendor/xiaomi-firmware/beryllium/xbl.img:install/firmware-update/xbl.img
